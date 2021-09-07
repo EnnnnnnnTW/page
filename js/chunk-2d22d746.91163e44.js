c4583fc6f3de6174233d448290a36d59dd42f5d1
ian <ian@faya.com.tw>
2021-09-07 18:20:51 +0800

    add about

---
+++ js/chunk-2d22d746.91163e44.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,n,e){"use strict";e.r(n);var u=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{attrs:{id:"About"}},[e("h3",[t._v("About")])])}],o={mounted:function(){},data:function(){return{}},computed:{},methods:{AddLecture:function(){}},components:{}},r=o,d=e("2877"),i=Object(d["a"])(r,u,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.91163e44.js.map
